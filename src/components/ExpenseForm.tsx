d496044f1733c402e53a8bb6da7caaf0420c67ae
Daniel <daniel.40.pompa@gmail.com>
2024-09-03 00:13:03 +0200

    feat: add expense form with date picker and categories
    - Added `ExpenseForm` component.
    - Integrated `DatePicker` component for date selection.
    - Included categories data and updated select input.

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,3 +1,77 @@
+import DatePicker from 'react-date-picker';
+import 'react-date-picker/dist/DatePicker.css';
+import 'react-calendar/dist/Calendar.css';
+import { categories } from '../data/categories';
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
 export const ExpenseForm = () => {
-  return <div>ExpenseForm</div>;
+  return (
+    <form className='space-y-5'>
+      <legend className='text-xl md:text-3xl text-slate-700 font-bold text-center border-b-4 border-blue-600 py-2'>
+        Nuevo Gasto
+      </legend>
+      <div className='flex flex-col gap-2'>
+        <label htmlFor='title' className='text-xl md:text-2xl text-slate-700 font-bold'>
+          Título
+        </label>
+        <input
+          type='text'
+          id='title'
+          className='w-full border border-slate-200 p-3 rounded focus:outline-none focus:border-slate-400'
+          name='title'
+          placeholder='Ejemplo: Comida'
+        />
+      </div>
+
+      <div className='flex flex-col gap-2'>
+        <label htmlFor='amount' className='text-xl md:text-2xl text-slate-700 font-bold'>
+          Importe
+        </label>
+        <input
+          type='number'
+          id='amount'
+          className='w-full border border-slate-200 p-3 rounded focus:outline-none focus:border-slate-400'
+          name='amount'
+          placeholder='Ejemplo: 300'
+        />
+      </div>
+
+      <div className='flex flex-col gap-2'>
+        <label
+          htmlFor='category'
+          className='text-xl md:text-2xl text-slate-700 font-bold'
+        >
+          Categoría
+        </label>
+        <select
+          id='category'
+          className='w-full border border-slate-200 p-3 rounded focus:outline-none focus:border-slate-400'
+          name='category'
+        >
+          <option value=''>-- Seleccione --</option>
+          {categories.map(category => (
+            <option key={category.id} value={category.id}>
+              {category.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className='flex flex-col gap-2'>
+        <label htmlFor='amount' className='text-xl md:text-2xl text-slate-700 font-bold'>
+          Fecha
+        </label>
+        <DatePicker name='date' />
+      </div>
+
+      <input
+        type='submit'
+        className='w-full bg-blue-600 hover:bg-blue-700 text-white p-2 cursor-pointer rounded transition-colors duration-500'
+        value={'Añadir Gasto'}
+      />
+    </form>
+  );
 };
---
+++ src/data/categories.ts
@@ -0,0 +1,14 @@
+export const categories = [
+  { id: '1', name: 'Casa', icon: 'home' },
+  { id: '2', name: 'Comida', icon: 'food' },
+  { id: '3', name: 'Salud', icon: 'health' },
+  { id: '4', name: 'Educación', icon: 'education' },
+  { id: '5', name: 'Ahorro', icon: 'money-saving' },
+  { id: '6', name: 'Ocio', icon: 'leisure' },
+  { id: '7', name: 'Gastos Varios', icon: 'expenses' },
+  { id: '8', name: 'Viajes', icon: 'travels' },
+  { id: '9', name: 'Transporte', icon: 'transport' },
+  { id: '10', name: 'Seguros', icon: 'insurances' },
+  { id: '11', name: 'Préstamos', icon: 'loans' },
+  { id: '12', name: 'Suscripciones', icon: 'subscriptions' },
+];
