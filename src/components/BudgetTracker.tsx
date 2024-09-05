a8db733612fb9bca0959a20fe8736f5b6db24f43
Daniel <daniel.40.pompa@gmail.com>
2024-09-06 01:12:15 +0200

    feat: add circular progress bar to BudgetTracker
    Integrates a circular progress bar to visualize budget usage.
    
    - Added `react-circular-progressbar` to display budget usage percentage.
    - Calculated percentage of the budget spent and converted it to a number.
    - Updated `BudgetTracker` to show a progress bar with dynamic styling based on budget usage.
    
    Improves user experience with visual representation of budget status.

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,13 +1,29 @@
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 import { useBudget } from '../hooks/useBudget';
 import { BudgetAmountDisplay } from './';
 
 export const BudgetTracker = () => {
   const { state, totalExpenseAmount, remainingBudget } = useBudget();
 
+  /** Calculates the percentage of the budget that has been spent */
+  const usagePercent = ((totalExpenseAmount / state.budget) * 100).toFixed(2);
+
+  const percentage = Number(usagePercent);
+
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
       <div className='flex justify-center'>
-        <img src='/chart.jpg' alt='Budget chart' />
+        <CircularProgressbar
+          value={percentage}
+          styles={buildStyles({
+            pathColor: percentage < 100 ? '#2563eb' : '#dc2626',
+            trailColor: '#e2e8f0',
+            textSize: 8,
+            textColor: percentage < 100 ? '#2563eb' : '#dc2626',
+          })}
+          text={`${percentage}% Gastado`}
+        />
       </div>
       <div className='flex flex-col justify-center items-center md:items-start gap-8'>
         <BudgetAmountDisplay label='Gastado' amount={totalExpenseAmount} />
